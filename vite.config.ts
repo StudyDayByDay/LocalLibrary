97dfbd57c983eebccc86ffddf526d6005c9041a3
lk <2995201114@qq.com>
2024-10-12 17:11:13 +0800

    ci: 新增环境变量配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,28 @@
-import {defineConfig} from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 import {resolve} from 'path';
 import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+export default defineConfig(({mode}) => {
+  // 加载对应环境的 .env 文件
+  const env = loadEnv(mode, process.cwd());
+  return {
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
     },
-  },
-  plugins: [
-    react(),
-    monacoEditorPlugin.default({
-      languageWorkers: ['editorWorkerService', 'typescript', 'css', 'html', 'json'], // 选择你需要的语言支持
-    }),
-  ],
-  base: '/LocalLibrary/',
-  server: {
-    host: true,
-    open: true,
-  },
+    plugins: [
+      react(),
+      monacoEditorPlugin.default({
+        languageWorkers: ['editorWorkerService', 'typescript', 'css', 'html', 'json'], // 选择你需要的语言支持
+      }),
+    ],
+    base: env.VITE_BASE_URL,
+    server: {
+      host: true,
+      open: true,
+    },
+  };
 });
